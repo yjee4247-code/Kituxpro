f95a15df4e94784b88ac3dc74a6571e40edd6276
ixrocky <shivamlkr629@gmail.com>
2025-12-01 02:08:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOGGER_ID = int(getenv("LOGGER_ID", -1002141133985))
 
 YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## xBit Music Endpoint.
-YT_API_KEY = getenv("YT_API_KEY" , None ) ## Your API key like: xbit_KRCKC908GB4TDG22OKBZD1 Get from  https://t.me/tgmusic_apibot
+YT_API_KEY = getenv("YT_API_KEY" , None ) ## Your API key like: xbit_9o85yscdt5YhD1JvDU9diSbq-2mzdfPe Get from  https://t.me/tgmusic_apibot
 
 # ======================================================
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
