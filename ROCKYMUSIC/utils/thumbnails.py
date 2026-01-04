16efad38138cb425fba26abdefc46af934c73cd7
yjee4247-code <yjee4247@gmail.com>
2026-01-05 01:34:31 +0530

    Update thumbnails.py

--- ROCKYMUSIC/utils/thumbnails.py
+++ ROCKYMUSIC/utils/thumbnails.py
@@ -4,7 +4,7 @@ import random
 import aiofiles
 import aiohttp
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
-from youtubesearchpython.__future__ import VideosSearch
+from py_yt import VideosSearch
 from config import YOUTUBE_IMG_URL
 from ROCKYMUSIC import app
 
